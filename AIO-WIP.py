3ff013c2570bf676fef797ee500223ba169005e1
omiinaya <omiinaya@gmail.com>
2020-01-28 16:44:57 -0500

    added magician section.

--- AIO-WIP.py
+++ AIO-WIP.py
@@ -2,11 +2,28 @@ answer = sm.sendNext("Pick a category: \r\n#L0#Magician Equips. \r\n#L1#Thief Eq
 
 
 if answer == 0:
-	MagicianEqupips = sm.sendSayOkay("\r\n#L0#Magician Shoes. \r\n#L1#Magician Overalls. \r\n#L2#Magician Gloves.")
-	if MagicianEqupips == 0:
-		sm.openShop(2080004) 
-	if MagicianEqupips == 1:
-		sm.sendSayOkay("test2.")
-
+	MagicianEquips = sm.sendSayOkay("\r\n#L0#Magician Shoes. \r\n#L1#Magician Overalls. \r\n#L2#Magician Gloves.\r\n#L3#Magician Hats. \r\n#L4#Magician Shields. \r\n#L5#Magician Wands. \r\n#L6#Magician Medals. \r\n#L7#Magician Face. \r\n#L8#Magician Cape.\r\n#L9#Magician Top. \r\n#L10#Magician Bottom. \r\n#L11#Magician ETC. \r\n#L12#Magician Staves.")
+	if MagicianEquips == 0:
+		sm.openShop(2080004) #replace with right shop id
+	if MagicianEquips == 1:
+		sm.openShop()
+	if MagicianEquips == 2:
+		sm.openShop()
+	if MagicianEquips == 3:
+		sm.openShop()
+	if MagicianEquips == 4:
+		sm.openShop()
+	if MagicianEquips == 5:
+		sm.openShop()
+	if MagicianEquips == 6:
+		sm.openShop()
+	if MagicianEquips == 7:
+		sm.openShop()
+	if MagicianEquips == 9:
+		sm.openShop()
+	if MagicianEquips == 10:
+		sm.openShop()
+	if MagicianEquips == 11:
+		sm.openShop()
 elif answer == 1:
-	sm.sendSayOkay("Warrior Equips: ")
+	#same for other jobs
